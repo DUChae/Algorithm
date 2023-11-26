53cf65506af72ada1a65108bd1e1510021425e92
DU <97602018+DUChae@users.noreply.github.com>
2023-11-26 14:59:06 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+function solution(seoul) {
+    var answer = '';
+    let test= 0 ; 
+    test = seoul.indexOf("Kim");
+    answer='김서방은'+" "+test+"에 있다";
+            
+        
+    
+    return answer;
+}
