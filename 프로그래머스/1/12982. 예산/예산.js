55d879551e6266fc4c63b1b849777b204f9646ab
DU <97602018+DUChae@users.noreply.github.com>
2023-11-28 10:38:53 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,10 @@
+function solution(d, budget) {
+    d.sort((a,b)=>a-b);
+    let count=0;
+    for(let i= 0 ; i<d.length;i++){
+        budget=budget-d[i];
+        if(budget<0) {break;}
+        count++
+    }
+    return count;
+}
