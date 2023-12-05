e6207c30606f6af44ed958fb61af0fb8239702bd
DU <97602018+DUChae@users.noreply.github.com>
2023-12-05 18:37:48 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,8 @@
+ const input = require('fs').readFileSync(0).toString().split('\n');
+const A=parseInt(input[0]);
+const B=parseInt(input[1]);
+    if(A>0&&B>0){console.log("1");}
+else if(A>0&&B<0){console.log("4");}
+else if(A<0&&B>0){console.log("2");}
+else {console.log("3");}
+
