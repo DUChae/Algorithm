744261de5bc52259cdcd3c6c5a4365198cbe3516
DU <97602018+DUChae@users.noreply.github.com>
2023-12-19 16:20:55 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    var answer = [];
+    let odd=0;
+    let even=0;
+    for(let i=0;i<num_list.length;i++){
+        if(num_list[i]%2==0){
+            odd+=1;
+        }else{even+=1;}
+    }
+  answer=[odd,even];
+    return answer;
+}
