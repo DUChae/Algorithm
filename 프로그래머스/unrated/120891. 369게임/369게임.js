37a51a7feba2e6ef29d33426993e27fe9f8a1009
DU <97602018+DUChae@users.noreply.github.com>
2023-12-22 17:16:31 +0900

    [level unrated] Title: 369게임, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120891. 369게임/369게임.js
@@ -0,0 +1,11 @@
+function solution(order) {
+    var answer = 0;
+    order=order.toString();
+    
+    for(let i=0;i<order.length;i++){
+        if(order[i]==='3'||order[i]==='6'||order[i]==='9'){
+            answer+=1;
+        }
+    }
+    return answer;
+}
