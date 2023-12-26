9e4f0b6ae5e41fca23e2142e7978a53072c495af
DU <97602018+DUChae@users.noreply.github.com>
2023-12-26 17:21:44 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var odd='';
+    var even='';
+    for(let i=0;i<num_list.length;i++){
+        if(num_list[i]%2!==0){odd+=num_list[i];}
+        else{even+=num_list[i];}
+    }
+    return Number(odd)+Number(even);
+    
+}
