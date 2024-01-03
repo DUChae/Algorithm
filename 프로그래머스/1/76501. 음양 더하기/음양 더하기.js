14f18976e57c517aff3a997870d91650cf159e49
DU <97602018+DUChae@users.noreply.github.com>
2024-01-03 15:50:25 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,18 +1,6 @@
 function solution(absolutes, signs) {
-    let result=[];
-    let sum=0;
-    for(let i = 0 ; i<absolutes.length;i++){
-        if(signs[i]==true){
-            //양수
-            result.push(absolutes[i]);
-        }
-        else{
-            //음수
-            result.push(absolutes[i]*-1);
-        }
-    }
-    for(let j = 0 ; j<absolutes.length;j++){
-        sum+=result[j];
-    }
-    return sum;
+    return absolutes.map((v,idx)=>
+        signs[idx]===true?v:-v
+    ).reduce((a,c)=>a+c);
+
 }
