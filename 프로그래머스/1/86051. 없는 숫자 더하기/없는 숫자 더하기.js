a0dcb86d17b004fd4c55d9aa99cd2992e7d89f7e
DU <97602018+DUChae@users.noreply.github.com>
2024-01-03 17:28:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,15 +1,3 @@
-function test(a,b){
-    return a-b;
-}
 function solution(numbers) {
-    let sum = 0;
-    const setup=[0,1,2,3,4,5,6,7,8,9];
-    let a=[];
-    a=numbers.sort(test);
-    let difference=setup.filter(x=>!numbers.includes(x));
-    console.log(difference);
-    for(let i=0 ; i<difference.length;i++){
-        sum+=difference[i];
-    }
-    return sum;
+    return 45-numbers.reduce((a,c)=>a+c);
 }
