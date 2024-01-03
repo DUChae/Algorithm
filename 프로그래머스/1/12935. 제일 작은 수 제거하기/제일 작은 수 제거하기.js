b09cbd30e2fa8dd08a459626becfe1a81fe64f17
DU <97602018+DUChae@users.noreply.github.com>
2024-01-03 17:38:14 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.01 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,12 +1,6 @@
 function solution(arr) {
-    var answer = [];
-    const minValue=Math.min.apply(null,arr);
-    for(let i = 0 ; i<arr.length;i++){
-        if(arr[i]===minValue){
-            arr.splice(i,1);
-            if(arr.length===0) {return [-1];}
-        }  
-    }
-  
-    return arr;
+   const result= arr.filter(item=>item!==Math.min(...arr));
+    if(result.length===0){result.push(-1)}
+   return result;
+ 
 }
