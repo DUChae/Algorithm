28723140742ad5803351ec18a552f82a89901fda
DU <97602018+DUChae@users.noreply.github.com>
2024-01-03 14:35:01 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,15 +1,22 @@
 function solution(s){
-    //p 확인
-    var regExpP=/p/ig;
-    let foundP= (s.match(regExpP) ||[]).length;
+//     //p 확인
+//     var regExpP=/p/ig;
+//     let foundP= (s.match(regExpP) ||[]).length;
     
     
-    //y 확인
-    var regExpY=/y/ig;
-    let foundY= (s.match(regExpY)||[]).length;
+//     //y 확인
+//     var regExpY=/y/ig;
+//     let foundY= (s.match(regExpY)||[]).length;
     
-    return foundP===foundY
+//     return foundP===foundY
+    let countP=0;
+        let countY=0;
     
+    const testing=s.split('').filter(item=>{
+        if(item==='p'||item==='P'){countP++;}
+        if(item==='y'||item==='Y'){countY++;}
+    });
+return countP===countY?true:false    
     
   //found에서 아무것도 찾지 못하여 Null값을 반환할 수 있다는 점을 명시해야한다. 
 }
