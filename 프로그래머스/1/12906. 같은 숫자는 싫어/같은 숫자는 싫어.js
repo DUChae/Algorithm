9ab69b3240104a7498845d496c7d790e71b47e16
DU <97602018+DUChae@users.noreply.github.com>
2024-01-04 15:13:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 42.71 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,5 @@
 function solution(arr)
 {
-    let answer=[];
-    for(let i =0 ; i<arr.length; i++){
-        if(i===0||arr[i]!==arr[i-1]){
-            answer.push(arr[i]);
-        }
-    }
-return answer;}
+    return arr.filter((item,index,array)=>item!==array[index+1]);
+    
+}
