1972060edd45026677e1cc1e0cbd53e90ce3329f
DU <97602018+DUChae@users.noreply.github.com>
2024-01-04 15:01:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,19 +1,3 @@
-// function solution(s) {
-//     var regex = /^[0-9]+$/;
-//     if(s.length==4 || s.length==6){
-//         for(let i = 0 ; i<s.length;i++){
-//             if(!regex.test(s[i])){
-//                 //test() 메서드는 주어진 문자열이 정규 표현식을 만족하는지 판별하고, 그 여부를 true 또는 false로 반환합니다.
-//                 // s[i]가 숫자가 아닌 경우의 처리
-
-//                 return false;
-//             }
-//             else{
-//                 return true;
-//             }
-//         }
-//     }
-// }
 function solution(s){
          var regex = /^[0-9]+$/;
 
