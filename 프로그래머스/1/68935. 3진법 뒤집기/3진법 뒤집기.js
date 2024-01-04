f7b9198ec7a17e6541870658dddba6cb50506dac
DU <97602018+DUChae@users.noreply.github.com>
2024-01-04 15:20:00 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,7 +1,5 @@
 function solution(n) {
-    var answer = 0;
-    let ThreeN=n.toString(3);
-    let ReverseThreeN=ThreeN.split("").reverse().join("");
-    let TenReverseThreeN= parseInt(ReverseThreeN,3);
-    return TenReverseThreeN;
+    
+    return parseInt(n.toString(3).split('').reverse().join(''),3);
+    
 }
