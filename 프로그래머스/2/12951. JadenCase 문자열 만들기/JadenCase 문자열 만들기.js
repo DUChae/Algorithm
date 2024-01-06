86d59a699d612f1c2884b3f3367ddd4dc31de943
DU <97602018+DUChae@users.noreply.github.com>
2024-01-06 15:50:41 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,31 +1,5 @@
-// function solution(s) {
-//     let str=s.split(" ");
-//     let regExp=/^[A-Za-z]/;
-//     for(let i =0 ; i<str.length;i++){
-//         if(regExp.test(str[i][0])){//첫글자가 문자인 경우
-//             //소문자일 때 -> 대문자
-//             if(str[i][0]===str[i][0].toLowerCase()){
-//                 str[i]=str[i][0].toUpperCase() + str[i].substring(1);
-//             }
-//             //대문자일 때 -> 그대로
-//             //첫글자 제외 나머지 중 대문자가 하나라도 있다면
-            
-//         }
-//         //첫글자가 숫자인 경우 -> 그대로
-//         str[i]=str[i][0]+str[i].substring(1).toLowerCase();
-//     }
-//     //join해야함
-//     return str.join(" ");
-// }
-//너무 어렵게 했나.... 런타임 에러가 뜬다...다시 해보자...
-
-function solution(s){
-    let str=s.split(" ");
-    for(let i =0  ; i<str.length; i++){
-        if(str[i]){
-            str[i]=str[i][0].toUpperCase() + str[i].substring(1).toLowerCase();
-        }
-        
-    }
-    return str.join(" ");
+function solution(s) {
+   return s.toLowerCase().split(' ').map(v=>v.charAt(0).toUpperCase()+v.substring(1)).join(' ')
+   
 }
+
