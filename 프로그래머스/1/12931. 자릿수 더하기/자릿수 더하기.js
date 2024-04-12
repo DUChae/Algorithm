bac3fa81e1a9cb9911508783419da143b29b3fc6
DU <97602018+DUChae@users.noreply.github.com>
2024-04-12 10:24:13 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,7 @@
 function solution(n)
-{
-    let sum=0;
-   while(n>0){
-       sum+=n%10;
-       n=Math.floor(n/10);
-   }
-
-    return sum;
-}
+{   let sum=0;
+    while(n>0){
+        sum+=n%10;
+        n=Math.floor(n/10);
+    }
+return sum;}
