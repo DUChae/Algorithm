69bd97e4f18baade114c8ce495c5498b839df0f7
DU <97602018+DUChae@users.noreply.github.com>
2024-04-18 15:14:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,7 +1,21 @@
 function solution(s) {
-    const str=s.toUpperCase().split(' ');
-    const result=str.map((v)=>{
-       return  v.split('').map((char,idx)=>idx%2!==0?char.toLowerCase():char).join('')
-    }).join(' ')
+    const answer=[];
+    let str=s.toLowerCase().split(' ');
+    for(let i=0;i<str.length;i++){
+        for(let k=0;k<str[i].length;k++){
+            if(k%2===0){
+                answer.push(str[i][k].toUpperCase());
+            }
+            else{answer.push(str[i][k])}
+            
+        }
+        if(i<str.length-1)
+        {answer.push(' ');}
+    }
+    const result=answer.join('');
+    
+    
+    
+    
     return result;
 }
